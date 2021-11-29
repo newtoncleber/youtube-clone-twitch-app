b215fbc936f71fe8e6e5fe922499a01d2b07c3ae
Newton Cleber Martins de Carvalho <newtoncleber@gmail.com>
2021-11-29 16:38:35 -0300

    first commit

---
+++ src/pages/CommingSoon/index.tsx
@@ -0,0 +1,13 @@
+import React, { ReactNode } from 'react';
+
+import { Wrapper, Headding } from './styles';
+
+function CommingSoon() {
+  return (
+    <Wrapper>
+      <Headding>CommingSoon...</Headding>
+    </Wrapper>
+  );
+};
+
+export default CommingSoon;
